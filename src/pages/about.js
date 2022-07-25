4d894451f926f8fc07425ad7fef2c9334b1ff12d
garethhough <garethhough@googlemail.com>
2022-07-25 17:40:38 +0100

    Arranged divs

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,21 +10,26 @@ const About = () => {
       <h1 className="Header1Styled">About</h1>
 
       <div className="Wrapper">
+        <div className="subTitles">
+          <h2>What is Code Station?</h2>
+          <h2>Who made this?</h2>
+          </div>
         <div className="whatIsCodestation">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            lacinia accumsan sagittis. Mauris gravida lorem eu nulla gravida
-            tempus. Nunc metus sapien, luctus et dui sagittis, cursus lacinia
-            arcu.{" "}
-          </p>
-          <p>
-            Donec elementum elit tristique mauris consectetur volutpat. Donec
-            eget nisl neque. Nullam quis nisl a turpis dignissim dapibus. In et
-            metus quam. Phasellus mollis pulvinar purus, at rhoncus dolor congue
-            nec. Vestibulum ante ipsum primis in faucibus orci luctus et
-            ultrices posuere cubilia curae; Phasellus at tortor ut tellus
-            lacinia eleifend.
-          </p>
+          <div className="whatIsText">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              lacinia accumsan sagittis. Mauris gravida lorem eu nulla gravida
+              tempus. Nunc metus sapien, luctus et dui sagittis, cursus lacinia
+              arcu. Nulla euismod, nisl eu consectetur consectetur, nisl nunc.
+            </p>
+            <p>
+              Donec elementum elit tristique mauris consectetur volutpat. Donec
+              eget nisl neque. Nullam quis nisl a turpis dignissim dapibus. In
+              et metus quam. Phasellus mollis pulvinar purus, at rhoncus dolor
+              congue nec.
+            </p>
+          </div>
+          <div className="whatIsImage"></div>
         </div>
         <div className="ConentFlexbox">
           <ContentItem />
