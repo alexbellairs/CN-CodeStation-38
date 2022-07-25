2a3c9359d362723c2ea6ff773a8b2eb7449ce2a8
Indykatz <cats83@me.com>
2022-07-25 20:36:51 +0100

    Kat header 1st edit

--- src/pageComponents/loginHeader1/loginHeader.styles.js
+++ src/pageComponents/loginHeader1/loginHeader.styles.js
@@ -6,10 +6,65 @@ export const LoginHeaderStyles = styled.div`
   padding: 10px;
   width: auto;
   text-align: left;
+  display: flex;
+  justify-content: right;
+`;
+
+export const LoginImgHolder = styled.div`
+  width: 250px;
+  height: 125px;
+  left: 148px;
+  top: 38px;
+  background: #e5ffee;
+  border-radius: 10px;
+  text-align: center;
 `;
 
 export const LoginImg = styled.img`
-  background-color: #e5ffee;
-  padding: 10px;
-  width: 150px;
+  padding: 2%;
+  height: 110px;
+  width: 222px;
 `;
+
+export const Circle1 = styled.div`
+  width: 100px;
+  height: 100px;
+  margin-left: 50px;
+  margin-top: 50px;
+  margin-bottom: 5px;
+  border-radius: 50%;
+  border: 10px solid #e5ffee;
+`;
+
+export const Circle2 = styled.div`
+  width: 100px;
+  height: 100px;
+  margin-left: 50px;
+  margin-top: 30px;
+  border-radius: 50%;
+  border: 10px solid #e5ffee;
+`;
+
+export const Circle3 = styled.div`
+  width: 100px;
+  height: 100px;
+  margin-left: 50px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border-radius: 50%;
+  border: 10px solid #e5ffee;
+`;
+
+export const Circle4 = styled.div`
+  width: 100px;
+  height: 100px;
+  margin-left: 50px;
+  margin-top: 50px;
+  margin-bottom: 20px;
+  border-radius: 50%;
+  border: 10px solid #e5ffee;
+`;
+
+// @media (max-width: 768px) {
+//   size: 50%;
+// }
