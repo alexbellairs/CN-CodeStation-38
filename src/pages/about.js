60dfe650d05c613219a480372f30dc3bf966c253
Dom Harbot <domharbot@gmail.com>
2022-07-26 16:20:46 +0100

    About page flexbox revision

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,38 +9,168 @@ const About = () => {
   return (
     <div className="AppStyles">
       <Quote />
-      <h1 className="styledH1">About</h1>
-      <div className="subTitles">
-        <h2 id="whatIsH2">
-          What is [{`{CODE}`},{`{STATION}`}]?
-        </h2>
-        <h2 id="whoMadeThisH2">Who made this?</h2>
-      </div>
-      <div className="Wrapper">
+
+    <div className = "AboutPageMainContainer">
+
+      {/* LEFT SECTION, What is Code Station */}
+
         <div className="whatIsCodestation">
-          <div className="whatIsText">
-            <p className="aboutP">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              lacinia accumsan sagittis. Mauris gravida lorem eu nulla gravida
-              tempus. Nunc metus sapien, luctus et dui sagittis, cursus lacinia
-              arcu. Nulla euismod, nisl eu consectetur consectetur, nisl nunc.
-            </p>
-            <p className="aboutP">
-              Donec elementum elit tristique mauris consectetur volutpat. Donec
-              eget nisl neque. Nullam quis nisl a turpis dignissim dapibus. In
-              et metus quam. Phasellus mollis pulvinar purus, at rhoncus dolor
-              congue nec.
-            </p>
+{/* TITLE */}
+          <div className="WhatIsTitle">What Is Code Station?</div>
+
+            <div className="TextAndPicture">
+            <div className="whatIsTextBox">
+              {/* COPY */}
+              <p className="aboutP">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                lacinia accumsan sagittis. Mauris gravida lorem eu nulla gravida
+                tempus. Nunc metus sapien, luctus et dui sagittis, cursus lacinia
+                arcu. Nulla euismod, nisl eu consectetur consectetur, nisl nunc.
+              </p>
+              <p className="aboutP">
+                Donec elementum elit tristique mauris consectetur volutpat. Donec
+                eget nisl neque. Nullam quis nisl a turpis dignissim dapibus. In
+                et metus quam. Phasellus mollis pulvinar purus, at rhoncus dolor
+                congue nec.
+              </p>
+              {/* PHOTO */}
+            </div>
+              <img id="prettyCodeLightImg" src={prettyCodeLight} />
+            </div>
           </div>
-          <div className="whatIsImage">
-            <img id="prettyCodeLightImg" src={prettyCodeLight} />
+
+
+
+{/* RIGHT SECTION */}
+
+          <div className="TeamProfiles">
+
+            <div className="WhoMadeThisTitle">Who made this?</div>
+
+
+              <div className="EveryonesProfilesBox">
+
+                <div className="ProfileBoxPadding">
+
+                  
+              <div className="TopThreeProfiles">
+{/* PROFILE 1 */}
+            <div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+{/* PROFILE 2 */}
+<div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+{/* PROFILE 3 */}
+<div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
+
+            <div className="MiddleThreeProfiles">
+
+            <div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+                <div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+                <div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+
+
+
+            </div>
+              
+
+
+            <div className="BottomThreeProfiles">
+
+
+
+
+            <div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+                <div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+                <div className="ProfileBox">
+                <div className="ProfilePicture">
+                </div>
+                  <div className="ProfileName">       
+                </div>
+                    <div className="ProfileButton">
+                    </div>
+                </div>
+
+            </div></div>
+
+          
+
+            </div>
+
+
+
+
+          {/* <ContentItem /> */}
           </div>
-        </div>
-        <div className="ConentFlexbox">
-          <ContentItem />
-        </div>
-      </div>
     </div>
+  </div>
+
   );
 };
 
