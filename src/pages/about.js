ea71ce3b123c34362e7403a061ad2f22f629ba86
garethhough <garethhough@googlemail.com>
2022-07-26 17:22:12 +0100

    Made changes to team profiles

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,40 +4,114 @@ import "../css/about.css";
 //import Quote from "../components/quote"; // nOT NEEDED?
 import prettyCodeLight from "../images/prettyCodeLight.jpg";
 import Quote from "../pageComponents/quote/quote";
+import blankFace from "../images/blankFace.png";
 
 const About = () => {
   return (
     <div className="AppStyles">
       <Quote />
-      <h1 className="styledH1">About</h1>
-      <div className="subTitles">
-        <h2 id="whatIsH2">
-          What is [{`{CODE}`},{`{STATION}`}]?
-        </h2>
-        <h2 id="whoMadeThisH2">Who made this?</h2>
-      </div>
-      <div className="Wrapper">
+
+      <div className="AboutPageMainContainer">
+        {/* LEFT SECTION, What is Code Station */}
+
         <div className="whatIsCodestation">
-          <div className="whatIsText">
-            <p className="aboutP">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              lacinia accumsan sagittis. Mauris gravida lorem eu nulla gravida
-              tempus. Nunc metus sapien, luctus et dui sagittis, cursus lacinia
-              arcu. Nulla euismod, nisl eu consectetur consectetur, nisl nunc.
-            </p>
-            <p className="aboutP">
-              Donec elementum elit tristique mauris consectetur volutpat. Donec
-              eget nisl neque. Nullam quis nisl a turpis dignissim dapibus. In
-              et metus quam. Phasellus mollis pulvinar purus, at rhoncus dolor
-              congue nec.
-            </p>
-          </div>
-          <div className="whatIsImage">
+          {/* TITLE */}
+          <div className="WhatIsTitle">What Is Code Station?</div>
+
+          <div className="TextAndPicture">
+            <div className="whatIsTextBox">
+              {/* COPY */}
+              <p className="aboutP">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                lacinia accumsan sagittis. Mauris gravida lorem eu nulla gravida
+                tempus. Nunc metus sapien, luctus et dui sagittis, cursus
+                lacinia arcu. Nulla euismod, nisl eu consectetur consectetur,
+                nisl nunc.
+              </p>
+              <p className="aboutP">
+                Donec elementum elit tristique mauris consectetur volutpat.
+                Donec eget nisl neque. Nullam quis nisl a turpis dignissim
+                dapibus. In et metus quam. Phasellus mollis pulvinar purus, at
+                rhoncus dolor congue nec.
+              </p>
+              {/* PHOTO */}
+            </div>
             <img id="prettyCodeLightImg" src={prettyCodeLight} />
           </div>
         </div>
-        <div className="ConentFlexbox">
-          <ContentItem />
+
+        {/* RIGHT SECTION */}
+
+        <div className="TeamProfiles">
+          <div className="WhoMadeThisTitle">Who made this?</div>
+
+          <div className="EveryonesProfilesBox">
+            <div className="ProfileBoxPadding">
+              <div className="TopThreeProfiles">
+                {/* PROFILE 1 */}
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">Kat</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+                {/* PROFILE 2 */}
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">Dom</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+
+                {/* PROFILE 3 */}
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">Wren</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+              </div>
+
+              <div className="MiddleThreeProfiles">
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">Alex B</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">Gaz</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">Karl</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+              </div>
+
+              <div className="BottomThreeProfiles">
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">James</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">Abdelbar</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+
+                <div className="ProfileBox">
+                  <img className="ProfilePicture" src={blankFace}></img>
+                  <div className="ProfileName">John</div>
+                  <button className="ProfileButton">Link to Git</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <ContentItem /> */}
         </div>
       </div>
     </div>
