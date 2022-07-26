750d357d84c7ea26995395bca6c20dbd0ddc7a11
Dom Harbot <domharbot@gmail.com>
2022-07-26 20:02:35 +0100

    Log in revised

--- src/pageComponents/loginHeader1/loginHeader.js
+++ src/pageComponents/loginHeader1/loginHeader.js
@@ -9,10 +9,6 @@ const LoginHeader = () => {
         <div className="LoginImgHolder">
           <img className="LoginImg" src={LoginLogo} alt="Square Logo" />
         </div>
-        <div className="Circle1"></div>
-        <div className="Circle2"></div>
-        <div className="Circle3"></div>
-        <div className="Circle4"></div>
       </div>
     </div>
   );
